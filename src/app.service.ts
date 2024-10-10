955bb6f85332cd11a19c3206bbc079a2f6594b2e
sid030sid <78762408+sid030sid@users.noreply.github.com>
2024-10-10 10:58:14 +0200

    starting adding pinata services

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,6 +6,10 @@ export class AppService {
     return 'Hello World!';
   }
 
+  createDid(): string {
+    return 'Creating DID';
+  }
+
   resolveDid(did: string): string {
     // get cid from did
     const cid = did.split(':')[2];
@@ -13,5 +17,24 @@ export class AppService {
     // retrieve data from public ipfs
     // if file not found, try private ipfs TODO: think about how the process of this looks like
     return `Resolving DID ${did}`;
+  }
+
+  //HELPER FUNCTIONS
+  //https://docs.pinata.cloud/quickstart-ipfs
+  uploadToIpfsViaPinata(file : string): string {
+    return 'Uploading to IPFS';
+  }
+
+  //https://docs.pinata.cloud/quickstart-files
+  uploadToPrivateIpfsViaPinata(file : string): string {
+    return 'Uploading to private IPFS';
+  }
+
+  downloadFromIpfsViaPinata(cid : string): string {
+    return 'Downloading from IPFS';
+  }
+
+  downloadFromPrivateIpfsViaPinata(cid : string): string {
+    return 'Downloading from private IPFS';
   }
 }
